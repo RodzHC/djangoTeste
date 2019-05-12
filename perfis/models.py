80b768b908a6af2f664d4247a41f2096351e3ef1
Rodrigo Marzullo <rodrigo.marzullo@gmail.com>
2019-05-12 11:15:23 -0300

    :tada: Addign Flask

--- perfis/models.py
+++ perfis/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
-class Perfil(object):
-    def __init__(self,nome='',email='',senha='',senhaRepete=''):
-        self.nome=nome
-        self.email=email
-        self.senha=senha
-        self.senhaRete=senhaRepete
+# python manage.py makemigrations
+# ->Comando para criar o arquivo de banco de dados basiado no schema
+# python manage.py migration
+# ->comando para implementar as migracoes que foram criadas com o makemigration
+
+class Perfil(models.Model):
+        nome = models.CharField(max_length=255, null=255)
+        email = models.CharField(max_length=255, null=255)
+        telefone = models.CharField(max_length=255, null=255)
+        nome = models.CharField(max_length=255, null=255)
+
 # Create your models here.
